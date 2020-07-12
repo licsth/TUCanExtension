b3c29b53cde021d320eb2984052d20dfbd25b1b2
Linda Thelen <lcs.thelen@gmail.com>
2020-07-12 21:29:46 +0200

    Change favicon

--- content.js
+++ content.js
@@ -1,3 +1,12 @@
 var div = document.createElement('div');
 div.classList = "sideOverlay";
 document.body.appendChild(div);
+
+var old = document.querySelectorAll('[rel="shortcut icon"]')[0];
+old.remove();
+
+var icon = document.createElement('link');
+icon.setAttribute('rel', 'icon');
+icon.setAttribute('type', 'image/x-icon');
+icon.setAttribute('href', chrome.runtime.getURL("favicon.ico"));
+document.head.appendChild(icon);
