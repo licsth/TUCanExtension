fed65c9b808024037c611d231ab23befafea6c1f
Jonas Bernard <public.jbernard@web.de>
2022-06-14 12:21:59 +0200

    Fix webmanifest

--- content.js
+++ content.js
@@ -13,7 +13,7 @@ addToHead(`<link rel="apple-touch-icon" sizes="180x180" href="${chrome.runtime.g
 + `<link rel="icon" type="image/x-icon" href="${chrome.runtime.getURL("icon/favicon.ico")}"`
 + `<link rel="icon" type="image/png" sizes="32x32" href="${chrome.runtime.getURL("icon/favicon-32x32.png")}">`
 + `<link rel="icon" type="image/png" sizes="16x16" href="${chrome.runtime.getURL("icon/favicon-16x16.png")}">`
-+ `<link rel="manifest" href="${chrome.runtime.getURL("site.manifest")}">`
++ `<link rel="manifest" href="${chrome.runtime.getURL("site.webmanifest")}">`
 + `<link rel="mask-icon" href="${chrome.runtime.getURL("icon/safari-pinned-tab.svg")}" color="#5bbad5">`
 + `<meta name="apple-mobile-web-app-title" content="TuCan\'t">`
 + `<meta name="application-name" content="TuCan\'t">`
